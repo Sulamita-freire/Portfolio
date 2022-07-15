606a0d0c0cc51b266190e9eab200b5a0619f68c7
Sulamita <sulamita.freire@hotmail.com>
2022-07-14 22:40:27 -0300

    ajuste

--- src/components/CarroselProject/index.jsx
+++ src/components/CarroselProject/index.jsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import imagem1 from "../../public/imagem1.png";
 import imagem2 from "../../public/imagem2.png";
 import imagem3 from "../../public/imagem3.png";
-import imagem4 from "../../public/imagem4.PNG";
+import imagem6 from "../../public/imagem6.png";
 import imagem5 from "../../public/imagem5.png";
 
 import './style.css'
@@ -21,7 +21,7 @@ export function CarroselProject() {
         <SwiperSlide> <Project img={imagem1} link="https://github.com/Sulamita-freire/Projeto-wireframe" title="Wireframe"/> </SwiperSlide>
         <SwiperSlide> <Project img={imagem2} link="https://github.com/Sulamita-freire/Layout-comunidade-Figma" title="Layout-comunidade-Figma"/> </SwiperSlide>
         <SwiperSlide> <Project img={imagem3} link="https://github.com/Sulamita-freire/Formulario" title="Fomulário"/> </SwiperSlide>
-        <SwiperSlide> <Project img={imagem4} link="https://github.com/Sulamita-freire/Rocket-Links" title="Rocket-Links"/> </SwiperSlide>
+        <SwiperSlide> <Project img={imagem6} link="https://github.com/Sulamita-freire/Rocket-Links" title="Rocket-Links"/> </SwiperSlide>
         <SwiperSlide> <Project img={imagem5} link="https://github.com/Sulamita-freire/OriginSix" title="OriginSix"/> </SwiperSlide>
       
       </Swiper>

9179c1e1c4c46eeeb65d9f0230ad01fa48837420
Sulamita Freire <96012046+Sulamita-freire@users.noreply.github.com>
2022-07-15 00:07:36 -0300

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import { Container } from "./style";
 
 export function Header() {
  return(
-    <Container >
+    <Container data-aos="fade-right" data-aos-easing="ease-in-sine" data-aos-duration="2000">
     <a href="https://github.com/Sulamita-freire"><GithubLogo size={32} /></a>
     <a href="https://www.linkedin.com/in/sulamitafreire/"><LinkedinLogo size={32} /></a>
     <a href="mailto:sulamita.freire@hotmail.com"><Envelope size={32} /></a>
